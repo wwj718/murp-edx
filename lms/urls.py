2b34f4aef8ea0cecdde003cbecd364ae0ecf19a1
wuwenjie <wwj@ubuntu.(none)>
2014-07-16 15:28:29 +0800

    替换themes logo

--- lms/urls.py
+++ lms/urls.py
@@ -20,8 +20,14 @@ urlpatterns = ('',  # nopep8
     url(r'^login$', 'student.views.signin_user', name="signin_user"),
     url(r'^register$', 'student.views.register_user', name="register_user"),
 
-    url(r'^wwjtest$','murp.views.murp_hello',name='murp_hello'),    
+    #edit by wwj
+    #url(r'^wwjtest$','murp.views.murp_hello',name='murp_hello'),    
 
+    url(r'^murp$','murp.views.murp_hello',name='murp_hello'),    
+    
+    url(r'^murp/2$','murp.views.murp_hello',name='murp_hello'),    
+    #end by wwj 
+    
     url(r'^admin_dashboard$', 'dashboard.views.dashboard'),
 
     url(r'^change_email$', 'student.views.change_email_request', name="change_email"),
